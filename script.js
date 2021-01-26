07db6b1efaf87afde7e7bb37be24760927ee11da
Sam Evans <scevans3452@yahoo.com>
2021-01-25 21:31:58 -0600

    Finished button functions

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let dateHolder = Date();
+let x = dateHolder.length;
+let count = 0;
 
+// for button 1
 function function1() {
     if (document.getElementById('target1').innerHTML != dateHolder)
     {
@@ -10,9 +13,20 @@ function function1() {
     }
 }
 
+// for button 2
 function changeDate() {
     dateHolder = Date();
     document.getElementById('target2').innerHTML = dateHolder;
+}
+
+// for button 3
+function dateLength() {
+    document.getElementById('target3').innerHTML = x;
+}
+
+function clickCount() {
+    count++;
+    document.getElementById('target4').innerHTML = count;
 }
 
 
